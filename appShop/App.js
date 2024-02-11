89849110aab604ecdd8db7ff99642baec9c5712c
Maksym <volomaksim72@gmail.com>
2024-02-11 03:14:29 +0000

    Co-authored-by: Dima Sribnyj <dimasribnyj14@users.noreply.github.com>

--- appShop/App.js
+++ appShop/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { useState } from 'react';
 import Product from './product';
 
 
 
-const host = ""
+const host = "https://testing021024.pythonanywhere.com/"
 const token = ''
 
 export default function App() {
@@ -27,19 +27,92 @@ export default function App() {
   fetchProducts()
 
   return (
-    <View style={styles.container}>
-      {productsList.map(product => (
-        <Product name={product.name} price={product.price} />
-      ))}
-    </View>
+    <ScrollView style={styles.page}>
+      <View style={styles.footerHeader}>
+        <View style={styles.shopName}>
+          <Text style={styles.shopNameText}>Meme shop :3</Text>
+        </View>
+
+        <TouchableOpacity>
+          <Image style={styles.imageStyle} source={require('./burgerMenu.png')}/>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.main}>
+        {productsList.map(product => (
+          <Product name={product.name} price={product.price} />
+        ))}
+      </View>
+      <View style={styles.footerHeader}>
+        <View style={styles.socialsIcons}>
+          <TouchableOpacity>
+            <Image style={styles.imageStyle} source={require('./discord.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.imageStyle} source={require('./youtube.png')}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.imageStyle} source={require('./instagram.png')}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.socialsTextView}>
+          <Text style={styles.socialsText}>+xxxxxxxxxx</Text>
+          <Text style={styles.socialsText}>xxxx@gmail.com</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#00f",
+  page: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
     width: "100%",
-    height: "100%"
+    height: "100%",
+    gap:10,
+
+  },
+  footerHeader:{
+    backgroundColor: "#190482",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+    flexDirection:"row",
+  },
+  shopName:{
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#7752FE"
+  },
+  shopNameText:{
+    color: "#fff",
+    fontSize: 32
+  },
+  imageStyle:{
+    width: 50,
+    height: 50
+  },
+  socialsIcons:{
+    flexDirection:"row",
+    gap: 10
+  },
+  socialsTextView:{
+    flexDirection:"column",
+    gap: 10,
+    justifyContent:"center",
+    alignItems: "flex-end",
+
+  },
+  socialsText:{
+    color: "#fff",
+    fontSize: 14
+  },
+  main:{
+    width:"90%",
+    alignItems: "center",
+    justifyContent: "top",
+    gap: 10,
+    padding: 10
   }
 })
--- appShop/product.jsx
+++ appShop/product.jsx
@@ -1,22 +1,74 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function Product({name, price}){
     return (
         <View style = {styles.container}>
-            <Text>{name}</Text>
-            <Text>{price}</Text>
-            <Text>Купить</Text>
+            <Image style={styles.imageStyle} source={{uri: 'https://i.imgflip.com/7p7zts.png',}}/>
+            <View style={styles.nameView}>
+                <Text style={styles.nameText}>{name}</Text>
+            </View>
+            <View style={styles.lowerView}>
+                <Text style={styles.lowerText}>{price}$</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.lowerText}>Buy</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#FFFFFF",
-        width: "200",
-        height: "300",
+        borderRadius: 10,
+        width: 250,
+        height: 360,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        paddingHorizontal: 10,
+        paddingVertical: 30,
+        gap: 10,
+        backgroundColor:"#8E8FFA",
+        borderColor: "#190482",
+        borderWidth: 2
+    },
+    imageStyle: {
+        width: 190,
+        height: 190,
+        borderRadius: 5,
+        borderColor: "#190482",
+        borderWidth: 2
+
+    },
+    nameView: {
+        borderRadius: 5,
+        backgroundColor: "#C2D9FF",
+        width: "90%",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10
+    },
+    nameText:{
+        color: "#190482",
+        fontSize: 20
+    },
+    lowerView:{
+        width: "90%",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection:"row",
+        paddingHorizontal: 10
+    },
+    lowerText:{
+        color: "#fff",
+        fontSize: 18
+    },
+    button:{
+        backgroundColor: "#7752FE",
+        borderRadius: 5,
+        borderColor: "#190482",
+        borderWidth: 2,
+        paddingHorizontal: 25,
+        paddingVertical: 5,
     }
 })
